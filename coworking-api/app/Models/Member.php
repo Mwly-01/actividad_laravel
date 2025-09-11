535b33d97cd30d1bd35e2947fbb91d9cc99c1d3f
mwly-01 <mlzardila@gmail.com>
2025-09-11 12:55:06 +0000

    feat: :sparkles: se esta termino las relaciones

--- coworking-api/app/Models/Member.php
+++ coworking-api/app/Models/Member.php
@@ -5,12 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use App\Models\Member;
 use App\Models\User;
-use App\Models\Plan;
 use App\Models\Booking;
 
-class Plan extends Model
+class Member extends Model
 {
     /** @use HasFactory<\Database\Factories\PlanFactory> */
     use HasFactory, SoftDeletes;
@@ -27,11 +25,6 @@ class Plan extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
-    }
-
-    public function plan()
-    {
-        return $this->belongsTo(Plan::class);
     }
 
     public function bookings()
--- coworking-api/app/Models/Space.php
+++ coworking-api/app/Models/Space.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\Room;
 
-class Plan extends Model
+class Space extends Model
 {
     /** @use HasFactory<\Database\Factories\PlanFactory> */
     use HasFactory, SoftDeletes;
