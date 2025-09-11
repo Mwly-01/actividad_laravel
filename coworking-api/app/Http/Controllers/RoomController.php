7966456cd84439f1946f706197b814a534bd913a
mwly-01 <mlzardila@gmail.com>
2025-09-11 18:07:01 +0000

    feat: :sparkles: ya no quieor hacer nada

--- coworking-api/app/Http/Controllers/RoomController.php
+++ coworking-api/app/Http/Controllers/RoomController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreRoomRequest;
 use App\Http\Requests\UpdateRoomRequest;
 use App\Models\Room;
+use Illuminate\Http\JsonResponse;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
 
 class RoomController extends Controller
 {
@@ -35,10 +38,16 @@ class RoomController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Room $room)
+    public function show(string $id): JsonResponse
     {
-        //
+        $result = Room::find($id);
+        if ($result) {
+            return $this->success(new PostResource($result), "Todo ok, como dijo el Pibe");
+        } else {
+            return $this->error("Todo mal, como NO dijo el Pibe", 404, ['id' => 'No se encontro el recurso con el id']);
+        }
     }
+    
 
     /**
      * Show the form for editing the specified resource.
